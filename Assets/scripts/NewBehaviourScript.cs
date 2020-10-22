337af370b658027a92a94389a7ce89a75dccb747
Jacob Lueke <515291@escg.ac.uk>
2020-10-22 13:40:34 +0100

    Og commit (includes links to tutorials and notes)

---
+++ Assets/scripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool amIAlive = true;
+    void Start()
+    {
+        Debug.Log("die");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
